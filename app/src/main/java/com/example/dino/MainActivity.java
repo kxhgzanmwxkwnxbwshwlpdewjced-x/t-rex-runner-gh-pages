cbdf6a018472b4c7e8bc6471feea610a9e2e8b21
kxhgzanmwxkwnxbwshwlpdewjced-x <o2020-00184@ucgdl.edu.mx>
2022-04-01 08:38:05 -0600

    Dino

---
+++ app/src/main/java/com/example/dino/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    String url="file:///android_asset/index.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.wv);
+        webView.getSettings() .setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+    }
+}
